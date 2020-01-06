3a6bf1f24e91f1798ce30e9bb89c989c7192e9e7
Your Name <samanbatool08@gmail.com>
2020-01-06 13:13:28 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(block) {
+    if (block > 43) {
+        return block - 42;
+    } else {
+        return 42 - block;
+    }
+}
+
+function distanceFromHqinFeet(distance) {
+    function distanceFromHqInBlocks(block) * 264
+}
